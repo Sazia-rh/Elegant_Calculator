a22f3de837e6e7fc5003873238ee976a60cdc00c
sazia <saziarahman09@gmail.com>
2021-11-16 23:59:37 +0600

    new java class added

---
+++ app/src/main/java/com/example/new_sample/SaziaRahman.java
@@ -0,0 +1,18 @@
+package com.example.new_sample;
+
+public class SaziaRahman {
+    int x;
+
+    public SaziaRahman(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
+
